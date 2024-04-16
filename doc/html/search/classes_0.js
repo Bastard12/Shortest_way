b199027ba7fa21c1f2d5326602df6accf1fd5e61
Bastard12 <103337840+Bastard12@users.noreply.github.com>
2024-04-16 13:57:17 +0300

    adding doxygen documentation
    doc

---
+++ doc/html/search/classes_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['edges_0',['Edges',['../struct_edges.html',1,'']]]
+];
---
+++ doc/html/search/functions_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['enter_0',['Enter',['../class_main_window.html#ae1b5ea57b786183c2c153525435dd29d',1,'MainWindow']]]
+];
